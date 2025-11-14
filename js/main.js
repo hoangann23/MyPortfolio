852796656dc3cd16fdc5af81fb727f4abd1596c7
Ann Luong <ahluong@Anns-MacBook-Pro.local>
2025-11-13 23:11:11 -0800

    first page

---
+++ js/main.js
@@ -0,0 +1,80 @@
+//Import the THREE.js library
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+// To allow for the camera to move around the scene
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+// To allow for importing the .gltf file
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+
+// Create a Three.JS Scene
+const scene = new THREE.Scene();
+// Create a new camera with positions and angles
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// Keep track of the mouse position, so we can make the model move
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
+
+// Keep the 3D object on a global variable so we can access it later
+let object;
+
+// OrbitControlsl allow the camera to move around the scene
+let controls;
+
+// Set which object to render
+let objToRender = "mock_object";
+
+// Instantiate a loader for the .gltf file
+const leader = new GLTFLoader();
+
+// Load the file
+loader.load(
+    `./models/${objToRender}/scene.gltf`,
+    function (gltf) {
+        // If the file is loaded, add it to the scene
+        object = gltf.scene;
+        scene.add(object);
+    },
+    function (xhr) {
+        // While it is loading, log the progress
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function (error) {
+        // If there is an error, log it
+        console.error(error);
+    }
+);
+
+// Instantiate a new renderer and set its size
+const renderer = new THREE.WebGLRenderer({ alpha: true }); // Alpha: true allows for the transparent background
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Add the renderer to the DOM
+document.getElementById("contaienr3D").appendChild(renderer.domElement);
+
+// Set how for the camera will be from the 3D model
+camera.position.z = 500;
+
+// Add lights to the scene, so we can actually see the 3D model
+const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
+topLight.position.set(500, 500, 500); //top-left-ish
+topLight.castShadow = true;
+scene.add(topLight);
+
+const ambientLight = new THREE.AmbientLight(0x333333, 1);
+scene.add(ambientLight);
+
+// Render the scene
+function animate() {
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
+}
+
+// Add a listener to the window, so we can resize the window and the camera
+window.addEventListener("resize", function () {
+    camera.aspect = this.window.innerWidth / this.window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(this.window.innerWidth, this.window.innerHeight);
+});
+
+animate();
